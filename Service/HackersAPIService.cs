86be4b2491b565e1ba03cd3899333e51878c3848
kamaraj889 <kamaraj@gmail.com>
2024-07-29 21:50:20 +0530

    Latest code

--- Service/HackersAPIService.cs
+++ Service/HackersAPIService.cs
@@ -2,20 +2,24 @@
 using HackersAPICore.Models;
 using Newtonsoft.Json;
 using System.Net;
+using BusinessLayer.Service;
 
 namespace HackersAPICore.Service
 {
-    public class HackersAPIService:IHackerService
+    public class HackersAPIService :IHackerService
     {
         public StoriesResponse GetStories()
         {
-            StoriesResponse response1 = new StoriesResponse();
-            var url = "https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty";//it should be the url of your api
-
-            string json = (new WebClient()).DownloadString(url);
-            return JsonConvert.DeserializeObject<StoriesResponse>(json);
+            HackersService hackersservice  = new HackersService();
+            var getstories= hackersservice.GetStories();
+            StoriesResponse storiesResponse = new StoriesResponse();
+            storiesResponse.title = getstories.title;
+            storiesResponse.time = getstories.time;
+            storiesResponse.score = getstories.score;
+            storiesResponse.by = getstories.by;
+            storiesResponse.id = getstories.id;
+            storiesResponse.descendants = getstories.descendants;
+            return storiesResponse;
         }
-
-     
     }
 }
